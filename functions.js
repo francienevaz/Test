58292cc67705a135639f6e617d9dcb85b36ee829
francienevaz <fran.vfraga@gmail.com>
2023-07-06 13:49:51 -0300

    updated

--- functions.js
+++ functions.js
@@ -1,14 +1,18 @@
-function plus () {
-    let x = 20;
-
-    console.log(x)
-
-    x = x + 30
-
-    
-    
+function novaSerie (nome, serie) {
+    return {nome, serie}
 }
 
-plus();
+let arr = [
+    novaSerie("Pedro", "The Last of us"),
+    novaSerie("João", "Wandinha"),
+    novaSerie("Weslley", "The Big Bang Theory"),
+    novaSerie("Bruno", "Smallville")
+]
 
+function seriePreferida (pessoa) {
+    return pessoa.nome + " sua série preferida é: " + pessoa.serie ;
+}
 
+console.log(arr.map(seriePreferida));
+
+// o método map cria uma nova array totalmente personalizada

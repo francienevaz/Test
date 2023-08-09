3568a6d3637eb1196c0d99ff8baf3c4203fc8c9b
francienevaz <fran.vfraga@gmail.com>
2023-08-09 00:02:15 -0300

    updated

--- Search_Company/script.js
+++ Search_Company/script.js
@@ -1,43 +1,43 @@
-document.addEventListener("DOMContentLoaded", function () {
-    
-const card = document.querySelector('.card');
-const companies = document.querySelector('.companies');
-const btnAdd = document.getElementById('add');
-const btnCancel = document.getElementById('cancelar');
-const btnAddCompany = document.getElementById('addCompany');
-const btnSearch = document.getElementById('btnFilter');
+document.addEventListener("DOMContentLoaded", function () {    
 
-const inputCompanyName = document.getElementById('company');
-const inputProprietaryName = document.getElementById('proprietary');
-const inputCNPJ = document.getElementById('cnpj');
-const inputSearch = document.getElementById("search");
+	const card = document.querySelector('.card');
+	const companies = document.querySelector('.companies');
+	const btnAdd = document.getElementById('add');
+	const btnCancel = document.getElementById('cancelar');
+	const btnAddCompany = document.getElementById('addCompany');
+	const btnSearch = document.getElementById('btnFilter');
 
-const statusActive = document.getElementById("ativa");
-const statusInative = document.getElementById("inativa");
-let statusCompany = "";
+	const inputCompanyName = document.getElementById('company');
+	const inputProprietaryName = document.getElementById('proprietary');
+	const inputCNPJ = document.getElementById('cnpj');
+	const inputSearch = document.getElementById("search");
 
-const messageFill = document.querySelector('.input-box span');
+	const statusActive = document.getElementById("ativa");
+	const statusInative = document.getElementById("inativa");
+	let statusCompany = "";
 
-    function checkClicked() {
-        if (statusActive.checked) {
-            statusInative.disabled = true;
-            statusActive.checked = false;
-            statusCompany = 'Ativa';
-        } else if (statusInative.checked) {
-            statusActive.disabled = true;
-            statusInative.checked = false;
-            statusCompany = 'Inativa';
-        } else {
-            statusCompany = '';
-            statusActive.disabled = false;
-            statusInative.disabled = false;
-        }
-    };
+	const messageFill = document.querySelector('.input-box span');
 
-    const addCompany = (company, proprietary, cnpj, status) => {
+	function checkClicked() {
+		if (statusActive.checked) {
+			statusInative.disabled = true;
+			statusActive.checked = false;
+			statusCompany = 'Ativa';
+		} else if (statusInative.checked) {
+			statusActive.disabled = true;
+			statusInative.checked = false;
+			statusCompany = 'Inativa';
+		} else {
+			statusCompany = '';
+			statusActive.disabled = false;
+			statusInative.disabled = false;
+		}
+	};
+
+	const addCompany = (company, proprietary, cnpj, status) => {
 	let datas = []; 
-	
-        if(localStorage.dataCompany) {
+
+		if(localStorage.dataCompany) {
 		datas = JSON.parse(localStorage.getItem('dataCompany'));
 	}
 
@@ -51,47 +51,47 @@ const messageFill = document.querySelector('.input-box span');
 	datas.push(newCompany);
 
 	card.style.display = "none";
-   	companies.style.display = "block";
-	
+	companies.style.display = "block";
+
 	// reset
 	inputCompanyName.value= "";
 	inputProprietaryName.value="";
 	inputCNPJ.value="";
 	statusActive.checked = false;
 	statusInative.checked= false;	
-	
+
 	localStorage.dataCompany = JSON.stringify(datas);
 
 	let data = JSON.parse(localStorage.dataCompany);
 
-   	data.forEach((cadastro)=>{
+	data.forEach((cadastro)=>{
 
 	let p = document.createElement("p");
-       	p.textContent = `Nome da Empresa: ${cadastro.name}`;
-        	companies.appendChild(p);
-        	p = document.createElement("p");
-        	p.textContent = `Representante: ${cadastro.representant}`;
-        	companies.appendChild(p);
-        	p = document.createElement("p");
-        	p.textContent = `CNPJ: ${cadastro.cnpj}`;
-        	companies.appendChild(p);
-        	p = document.createElement("p");
-        	p.textContent = `Status: ${cadastro.status}`;
-        	companies.appendChild(p);
-    	});
+		p.textContent = `Nome da Empresa: ${cadastro.name}`;
+			companies.appendChild(p);
+			p = document.createElement("p");
+			p.textContent = `Representante: ${cadastro.representant}`;
+			companies.appendChild(p);
+			p = document.createElement("p");
+			p.textContent = `CNPJ: ${cadastro.cnpj}`;
+			companies.appendChild(p);
+			p = document.createElement("p");
+			p.textContent = `Status: ${cadastro.status}`;
+			companies.appendChild(p);
+		});
 
- };
+	};
 
-const search = (input) => {
+	const search = async (input) => {
 
 	let data = []; 
-	
-        if (localStorage.dataCompany) {
-		data = JSON.parse(localStorage.getItem('dataCompany'));
+
+		if (localStorage.dataCompany) {
+		data = await JSON.parse(localStorage.getItem('dataCompany'));
 	}
 
 	let found = false;
-	
+
 	companies.textContent = ""; 
 
 	for (let  i in data) {
@@ -103,19 +103,19 @@ const search = (input) => {
 		if (name == input || proprietary == input || cnpj == input || status == input) {
 			
 			let p = document.createElement("p");
-            p.textContent = `Nome da Empresa: ${name}`;
-            companies.appendChild(p);
-            p = document.createElement("p");
-            p.textContent = `Representante: ${proprietary}`;
-            companies.appendChild(p);
-            p = document.createElement("p");
-            p.textContent = `CNPJ: ${cnpj}`;
-            companies.appendChild(p);
-            p = document.createElement("p");
-            p.textContent = `Status: ${status}`;
-            companies.appendChild(p);
+			p.textContent = `Nome da Empresa: ${name}`;
+			companies.appendChild(p);
+			p = document.createElement("p");
+			p.textContent = `Representante: ${proprietary}`;
+			companies.appendChild(p);
+			p = document.createElement("p");
+			p.textContent = `CNPJ: ${cnpj}`;
+			companies.appendChild(p);
+			p = document.createElement("p");
+			p.textContent = `Status: ${status}`;
+			companies.appendChild(p);
 			found = true;
-        			
+					
 		} 
 	}
 
@@ -123,36 +123,36 @@ const search = (input) => {
 		let p = document.createElement("p");
 		p.textContent= `${input} não consta em nosso sistema, clique em "Adicionar" para fazer o seu cadastro!`;
 		companies.appendChild(p);
-}
-};
+	}
+	};
 
-inputCompanyName.addEventListener("blur", () => {
-    messageFill.classList.remove('hidden');
-});    
-inputProprietaryName.addEventListener("blur", () => {
-    messageFill.classList.remove('hidden');
-});      
-inputCNPJ.addEventListener("blur", () => {
-    messageFill.classList.remove('hidden');
-});
+	inputCompanyName.addEventListener("blur", () => {
+	messageFill.classList.remove('hidden');
+	});    
+	inputProprietaryName.addEventListener("blur", () => {
+	messageFill.classList.remove('hidden');
+	});      
+	inputCNPJ.addEventListener("blur", () => {
+	messageFill.classList.remove('hidden');
+	});
 
-btnAddCompany.addEventListener("click", (event) => {
-    event.preventDefault();
-    addCompany(inputCompanyName.value, inputProprietaryName.value, inputCNPJ.value, statusCompany);
-});
-    
+	btnAddCompany.addEventListener("click", (event) => {
+	event.preventDefault();
+	addCompany(inputCompanyName.value, inputProprietaryName.value, inputCNPJ.value, statusCompany);
+	});
 
-btnAdd.addEventListener("click", () => {
-    card.style="display: block";
-});
-btnCancel.addEventListener("click",() =>{
-    location.reload();
-});
 
-btnSearch.addEventListener("click", (event) => {
-    event.preventDefault();
-    search(inputSearch.value.toLowerCase());
-});
+	btnAdd.addEventListener("click", () => {
+	card.style="display: block";
+	});
+	btnCancel.addEventListener("click",() =>{
+	location.reload();
+	});
+
+	btnSearch.addEventListener("click", (event) => {
+	event.preventDefault();
+	search(inputSearch.value.toLowerCase());
+	});
 });
 
 

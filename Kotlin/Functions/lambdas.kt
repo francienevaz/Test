92e27d282a640c09e14b231b4754f1954d4608b9
francienevaz <fran.vfraga@gmail.com>
2023-09-20 20:50:04 -0300

    updated

---
+++ Kotlin/Functions/lambdas.kt
@@ -0,0 +1,21 @@
+fun main() {
+    // todos são exemplos de lambdas functions
+
+    val upperCase1: (String) -> String = { str: String -> str.uppercase() }
+
+    val upperCase2: (String) -> String = { str -> str.uppercase() }        
+
+    val upperCase3 = { str: String -> str.uppercase() }                    
+
+    // val upperCase4 = { str -> str.uppercase() }   precisar tipar para que funcione
+
+    val upperCase5: (String) -> String = { it.uppercase() }                
+
+    val upperCase6: (String) -> String = String::uppercase                 
+
+    println(upperCase1("hello"))
+    println(upperCase2("hello"))
+    println(upperCase3("hello"))
+    println(upperCase5("hello"))
+    println(upperCase6("hello"))
+}
